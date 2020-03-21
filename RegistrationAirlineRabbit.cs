b423abc0d50902b3688300904315f350f78676b5
noamori25 <44751354+noamori25@users.noreply.github.com>
2020-03-21 20:50:39 +0200

    Add files via upload

---
+++ RegistrationAirlineRabbit.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using ProjectManagmentSystem.POCO;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace ProjectManagmentSystem.BLL
+{
+   public static class RegistrationAirlineRabbit
+    {
+        public static void SendToRabbitAirline (AirlineCompany airline)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("Register airline", false, false, false, null);
+                byte[] body = Encoding.Default.GetBytes(JsonConvert.SerializeObject(airline));
+                channel.BasicPublish("", "Register airline", null, body);
+            }
+
+        }
+    }
+}
